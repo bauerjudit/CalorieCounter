d4165b77340108e3785851c5d61ff3e5d7e59357
Judit Bauer <judit.bauer@yahoo.com>
2016-01-25 12:46:03 +0100

    first attempt to build a calorie counter app

---
+++ items.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'caloriecounter'
+});
+
+connection.connect();
+
+
+function getMeals(callback) {
+	connection.query('SELECT * FROM caloriecounter', function(err, result) {
+		if (err) throw err;
+		callback(result);
+	});
+}
+
+
+function getOneMeal(id, callback) {
+	connection.query('SELECT * FROM caloriecounter WHERE id = ?', id, function(err, result) {
+		if (err) throw err;
+		var item = result[0];
+		callback(item);
+	});
+}
+
+module.exports = {
+  all: getMeals,
+  getOneMeal: getOneMeal
+};
---
+++ server.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var items = require("./items.js");
+
+var app = express();
+
+app.use(logRequest);
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+app.listen(3000, function() {
+	console.log("Listening on port 3000...")
+});
+
+
+app.get("/meals", function(req, res) {
+	items.all(function (result) {
+		res.status(200).json(result);
+	});
+});
+
+
+app.get("/meals/:id", function(req, res) {
+	var id = parseInt(req.params.id);
+	console.log(id);
+	items.getOneMeal(id, function (item) {
+		res.status(200).json(item);
+	});
+});
+
+
+
+function logRequest(req, res, next) {
+  var parts = [
+    new Date(),
+    req.method,
+    req.originalUrl,
+  ];
+  console.log(parts.join(" "));
+  next();
+};
+
