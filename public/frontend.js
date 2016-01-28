197162fbe19480e54e8f099487d21733797b5d2c
Judit Bauer <judit.bauer@yahoo.com>
2016-01-28 12:13:17 +0100

    fix duplicate bug

--- public/frontend.js
+++ public/frontend.js
@@ -2,17 +2,17 @@
 
 var url = "http://localhost:3000/meals";
 
-var addButton = document.querySelector(".button");
+var addButton = document.querySelector(".addButton"); // 
 var consumedCalories = document.querySelector(".consumedCalories");
 var filterByDateButton = document.querySelector(".filterDate");
 var listOfMeals = document.querySelector(".listMeals");
-var listAllButton = document.querySelector(".listAll");
-var mealNumber = document.querySelector(".numberOfMeals")
-var sumCalories = document.querySelector(".sumCalories");
-var textAreaMeal = document.querySelector(".addMeal");
-var textAreaCalories = document.querySelector(".addCalories");
-var textAreaDate = document.querySelector("#date");
-var textAreaFilterDate = document.querySelector(".filterByDate");
+var listAllButton = document.querySelector(".listAll"); //
+var mealNumber = document.querySelector(".numberOfMeals") //
+var sumCalories = document.querySelector(".sumCalories"); //
+var textAreaMeal = document.querySelector(".addMeal"); //
+var textAreaCalories = document.querySelector(".addCalories"); //
+var textAreaDate = document.querySelector(".addDate"); //
+var textAreaFilterDate = document.querySelector(".filterByDate"); //
 
 
 function getDateFromServer(url, callback) {
@@ -29,8 +29,14 @@ function getDateFromServer(url, callback) {
 	}
 }
 
+function refresCallback() {
+	clears();
+	listMealsFromServer(listMealsOnHtmlTable);
+}
+
 function clears() {
-	listOfMeals.innerText = "";
+	while (listOfMeals.firstChild) {listOfMeals.removeChild(listOfMeals.firstChild)}
+     // listOfMeals.innerHtml = "";
 }
 
 
@@ -68,15 +74,16 @@ function numberOfMeals(res) {
 
 
 function listAllMeals() {
-	listMealsFromServer(listMealsOnHtml)
+	//listMealsFromServer(listMealsOnHtml)
+	listMealsFromServer(listMealsOnHtmlTable);
 }
 
 function postNewMealToServer(callback) {
 	var req = new XMLHttpRequest();
 	req.open("POST", url);
 	req.setRequestHeader('Content-Type', 'application/json');
-	req.send(JSON.stringify({name: textAreaMeal.value, calories: textAreaCalories.value, date: textAreaDate.value}));
 	console.log(JSON.stringify({name: textAreaMeal.value, calories: textAreaCalories.value, date: textAreaDate.value}))
+	req.send(JSON.stringify({name: textAreaMeal.value, calories: textAreaCalories.value, date: textAreaDate.value}));
 	console.log("start");
 	req.onreadystatechange = function () {
 		if(req.readyState === 4) {
@@ -98,19 +105,46 @@ function listMealsFromServer(callback) {
 			var res = JSON.parse(req.response);
 			console.log("response ok");
 			console.log(res)
-			return callback(res);
+			callback(res);
 		}
 	}
 }
 
+function listMealsOnHtmlTable(res) {
+	var meals = res;
+	meals.forEach(function(meal) {
+		addMealsToHtmlTable(meal);
+	});
+}
 
-function listMealsOnHtml(res) {
+
+
+function addMealsToHtmlTable(meal) {
+	var tableElement = listMealInTable(meal);
+	listOfMeals.innerHTML += tableElement;
+}
+
+
+
+function listMealInTable(meal) {
+	console.log(meal);
+	var date = meal.date.split("T")[0];
+	var line = `<tr id="${meal.id}">
+                   <td>${meal.name}</td>
+                   <td>${meal.calories}</td>
+                   <td>${date}</td>
+                 </tr>`;
+     return line;
+}
+
+
+/*function listMealsOnHtml() {
 	res.forEach(function(meal) {
 		var newMeal = document.createElement("p");
 		newMeal.innerText = meal.name + " " + meal.calories + " " + meal.date.split("T")[0];
 		listOfMeals.appendChild(newMeal);
 	});
-}
+}*/
 
 
 listAllMeals();
@@ -123,14 +157,15 @@ listMealsFromServer(countConsumedCalories);
 
 addButton.addEventListener("click", function() {
 	clears();
-	postNewMealToServer(listAllMeals);
+	postNewMealToServer(refresCallback);
+	
 });
 
 
 filterByDateButton.addEventListener("click", function() {
 	var newUrl = url + "/filter/" + textAreaFilterDate.value;
 	clears();
-	getDateFromServer(newUrl, listMealsOnHtml);
+	getDateFromServer(newUrl, listMealsOnHtmlTable);
 	getDateFromServer(newUrl, sumAllTheCalories);
 	getDateFromServer(newUrl, numberOfMeals);
 	getDateFromServer(newUrl, countConsumedCalories);
